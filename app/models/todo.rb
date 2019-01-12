ae8de8b962b2bc06d519bc528f6fbe91afd74e54
Makoto Nihei <nihemak@gmail.com>
2019-01-13 00:05:59 +0900

    Fix table name

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,6 @@
 class Todo
   include Dynamoid::Document
 
-  table name: "#{ENV[:DYNAMO_PREFIX]}-todos", key: :id
+  table name: "#{ENV['DYNAMO_PREFIX']}-todos", key: :id
   field :text, :string
 end
